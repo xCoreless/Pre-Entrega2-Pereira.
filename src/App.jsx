e6ce87ccc08bb8798db6cdadc656a874925f9a4e
xCoreless <144195208+xCoreless@users.noreply.github.com>
2024-05-01 19:13:07 -0400

    update manual
    update manual de proyecto, error con los tokens de autenticación

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+import NavBar from './Components/NavBar'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
+
+
+function App() {
+    return (
+  <>
+    <NavBar/>
+    <Main/>
+    <Footer/>
+   </>
+  )   
+}
+
+export default App
